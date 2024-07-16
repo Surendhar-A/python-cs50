542932b53349a4c478f142246c37014b78c2902a
Surendhar <surendharbabu.a@gmail.com>
2024-07-16 12:33:13 +0530

    Week-3 taqueria

--- Week-3/ProblemSet-3/taqueria.py
+++ Week-3/ProblemSet-3/taqueria.py
@@ -10,4 +10,16 @@ def main():
         "Taco": 3.00,
         "Tortilla Salad": 8.00
     }
-    
+    total_cost = 0
+    while True:
+        try:
+            get_input = input("Item: ").title()
+            if get_input in items:
+                total_cost += items[get_input]
+                print(f"Total: ${total_cost:.2f}")
+            else:
+                pass
+        except EOFError:
+            print()
+            break
+main()
